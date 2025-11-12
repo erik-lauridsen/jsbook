8cd30db546d18a4c979674b914912dbb88605117
Erik Lauridsen <elauridsen03@gmail.com>
2025-11-12 12:47:03 -0500

    First commit.  Using esbuild to bundle jsx in the browser, as well as some limited support for css.

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,63 @@
+import * as esbuild from 'esbuild-wasm';
+import localForage from 'localforage';
+import axios from 'axios';
+
+const fileCache = localForage.createInstance({
+  name: 'fileCache',
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: 'fetch-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js)/ }, (args: any) => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const cachedResult: esbuild.OnLoadResult | null =
+          await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult;
+        }
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const url = new URL('./', request.responseURL).pathname;
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = \`${escaped}\`;
+          document.head.appendChild(style);
+          `;
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: contents,
+          resolveDir: url,
+        };
+        await fileCache.setItem<esbuild.OnLoadResult>(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const url = new URL('./', request.responseURL).pathname;
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
+          resolveDir: url,
+        };
+        await fileCache.setItem<esbuild.OnLoadResult>(args.path, result);
+        return result;
+      });
+    },
+  };
+};
